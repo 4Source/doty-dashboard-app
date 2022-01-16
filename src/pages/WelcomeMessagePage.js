78544998b4dca7d23d2e03de11f52082853b6e04
4Source <4Source@users.noreply.github.com>
2022-01-16 02:35:14 +0100

    Create WelcomeMessagePage Design

--- src/pages/WelcomeMessagePage.js
+++ src/pages/WelcomeMessagePage.js
@@ -1,9 +1,34 @@
-import { Page } from "../utils/styles";
+import { Button, ContainerStyle, Flex, Page, Select, TextArea, Title } from "../utils/styles";
 
 export const WelcomeMessagePage = () => {
     return (
         <Page>
-            WelcomeMessagePage
+            <ContainerStyle>
+                <Title>Update Welcome Message</Title>
+                <div>
+                    <section>
+                        <div>
+                            <label>Current Channel</label>
+                        </div>
+                        <Select style={{ margin: '10px 0px' }}>
+                            <option disabled>Please Select a Channel</option>
+                            <option>123</option>
+                            <option>124</option>
+                            <option>125</option>
+                        </Select>
+                    </section>
+                    <section style={{ margin: '10px 0px' }}>
+                        <div>
+                            <label htmlFor="message">Current Message</label>
+                        </div>
+                        <TextArea style={{ margin: '10px 0px' }} id="message"/>
+                    </section>
+                    <Flex justifyContent="flex-end">
+                        <Button type="button" style={{marginRight: '8px'}}>Reset</Button>
+                        <Button primary>Save</Button>
+                    </Flex>
+                </div>
+            </ContainerStyle>
         </Page>
     );
 }
--- src/utils/styles/index.jsx
+++ src/utils/styles/index.jsx
@@ -122,6 +122,27 @@ export const InputField = styled.input`
     }
 `;
 
+export const TextArea = styled.textarea`
+    padding: 10px 16px;
+    box-sizing: border-box;
+    font-size: 16px;
+    color: inherit;
+    font-family: inherit;
+    background-color: #2121218d;
+    border-radius: 5px;
+    border: 1px solid #58585858;
+    outline: none;
+    width: 100%;
+    box-shadow: 0px 1px 5px 0px #00000030;
+    resize: vertical;
+    min-height: 45px;
+
+    &:focus {
+        outline: 1px solid #585858F0;
+        box-shadow: 0px 1px 5px 0px #000000F0;
+    }
+`;
+
 export const Button = styled.button`
     background: ${props => props.primary ? "#006ed3" : "#3d3d3d"};
     padding: 10px 24px;
@@ -131,9 +152,28 @@ export const Button = styled.button`
     font-size: 16px;
     color: inherit;
     font-family: inherit;
-    
+    cursor: pointer;
 `;
 
 export const Page = styled.div`
     padding: 35px;
+`;
+
+export const Select = styled.select`
+    width: 100%;
+    padding: 10px 24px;
+    font-family: inherit;
+    font-size: 16px;
+    background-color: #3d3d3d;
+    color: inherit;
+    border: 1px solid #58585858;
+    border-radius: 5px;
+
+    &:focus {
+        outline: none;
+    }
+
+    & > option{
+        background-color: inherit;
+    }
 `;
