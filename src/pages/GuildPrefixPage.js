b467f846840757f2909f60585a582293d800c319
4Source <4Source@users.noreply.github.com>
2022-01-24 18:36:49 +0100

    Use Fetched Guild Config in GuildPrefixPage

--- src/pages/GuildPrefixPage.js
+++ src/pages/GuildPrefixPage.js
@@ -1,22 +1,40 @@
+import { useContext } from "react";
+import { BarLoader } from "react-spinners";
+import { GuildContext } from "../utils/contexts/GuildContext";
+import { useGuildConfig } from "../utils/hooks/useGuildConfig";
 import { Button, ContainerStyle, Flex, InputField, Page, Title } from "../utils/styles";
 
 export const GuildPrefixPage = () => {
+    const { guild } = useContext(GuildContext);
+    const [ config, error, loading ] = useGuildConfig((guild && guild.id) || '');
+
     return (
         <Page>
             <ContainerStyle>
-                <div>
-                    <Title>Update Command Prefix</Title>
-                    <form>
-                        <div>
-                            <label htmlFor="prefix">Current Prefix</label>
-                        </div>
-                        <InputField style={{ margin: '10px 0px' }} id="prefix"/>
-                        <Flex justifyContent="flex-end">
-                            <Button type="button" style={{marginRight: '8px'}}>Reset</Button>
-                            <Button primary>Save</Button>
+                {!loading && config && !error ? (
+                    <>
+                        <Title>Update Command Prefix</Title>
+                        <form>
+                            <div>
+                                <label htmlFor="prefix">Current Prefix</label>
+                            </div>
+                            <InputField style={{ margin: '10px 0px' }} id="prefix" placeholder={config.prefix}/>
+                            <Flex justifyContent="flex-end">
+                                <Button type="button" style={{marginRight: '8px'}}>Reset</Button>
+                                <Button primary>Save</Button>
+                            </Flex>
+                        </form>
+                    </>
+                ) : (error ? (
+                        <div>Error</div>
+                    ) : (
+                        <Flex justifyContent="center">
+                            <BarLoader color="white" />
                         </Flex>
-                    </form>
-                </div>
+                    )
+                )
+
+                }
             </ContainerStyle>
         </Page>
     );
